cd7943a85220d87d10750e6f46f72feb5d26ce25
Catherine Karra and Donna Ditz <donna@>
2016-03-22 15:03:13 -0700

    switch background and text colors

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+$(document).ready(function() {
+  $("button#green").click(function() {
+    $("body").removeClass();
+    $("body").addClass("green-background");
+  });
+
+  $("button#yellow").click(function() {
+    $("body").removeClass();
+    $("body").addClass("yellow-background");
+  });
+
+  $("button#red").click(function() {
+    $("body").removeClass();
+    $("body").addClass("red-background");
+  });
+
+  $("button#black").click(function() {
+    $("body").removeClass();
+    $("body").addClass("black-background").t;
+    $("body").addClass("text-white");
+  });
+
+  $("button#white").click(function() {
+    $("body").removeClass();
+    $("body").addClass("white-background");
+    $("body").addClass("text-black");
+  });
+
+});
